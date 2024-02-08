6dcbd0389eba7eb0b49cea6c442d5803c4168b71
ryouji <helloryouji@outlook.com>
2024-02-08 16:57:55 +0800

    fixed bug on visual mode

--- plugin/ccount.lua
+++ plugin/ccount.lua
@@ -7,61 +7,51 @@ function get_buffer_text()
 end
 
 function get_selected_text()
-	local start_pos = vim.fn.getpos("'<")
-	local end_pos = vim.fn.getpos("'>")
-	
-	local start_line = start_pos[2]
-	local end_line = end_pos[2]
-
-	local selected_lines = vim.fn.getline(start_line, end_line)
-
-	-- If no content is selected, return directly
-	if not selected_lines or #selected_lines == 0 then
-		return nil
+	local a_orig = vim.fn.getreg('a')
+	local mode = vim.fn.mode()
+	if mode ~= 'v' and mode ~= 'V' then
+		vim.cmd([[normal! gv]])
 	end
-
-	if start_line == end_line then
-		selected_lines[1] = string.sub(selected_lines[1], start_pos[3], end_pos[4])
-	else
-		for i, line in ipairs(selected_lines) do
-			local start_col = (i == 1) and start_pos[3] or 1
-			local end_col = (i == #selected_lines) and end_pos[4] or #line
-			if i == #selected_lines then
-				-- 对于选中的最后一行，修正 end_col 为字符串末尾位置
-				end_col = #line
-			end
-			selected_lines[i] = string.sub(line, start_col, end_col)
-		end
-	end
-	
-	local text = table.concat(selected_lines, '\n')
+	vim.cmd([[silent! normal! "aygv]])
+	local text = vim.fn.getreg('a')
+	vim.fn.setreg('a', a_orig)
 
 	return text
 end
 
 function count_chinese_characters(str)
-  local wordCount = 0
-  local charCount = 0
+	local wordCount = 0
+	local charCount = 0
 
-  for word in str:gmatch("%w+") do
-    wordCount = wordCount + 1
-  end
+	if str ~= nil and str ~= "" then
+		-- Iterate over each word in the string
+		for word in str:gmatch("%w+") do
+			wordCount = wordCount + 1
+		end
+	end
 
-  for utf8char in str:gmatch("[\226-\233][\128-\191]") do
-    charCount = charCount + 1
-  end
+	if str ~= nil and str ~= "" then
+		for utf8char in str:gmatch("[\226-\233][\128-\191]") do
+			charCount = charCount + 1
+		end
+	end
 
-  local result = wordCount + charCount
-  print(result)
+	local result = wordCount + charCount
+	print(result)
 end
 
 function count_cc()
-	local text = get_selected_text()
-
-	if text then
+	local mode = vim.api.nvim_get_mode()["mode"]
+	-- print(mode)
+	-- print(text)
+	if mode == "v" then
+		local text = get_selected_text()
+		count_chinese_characters(text)
+	elseif mode == "n" then
+		local text = get_buffer_text()
 		count_chinese_characters(text)
 	else
-		text= get_buffer_text()
+		local text = get_buffer_text()
 		count_chinese_characters(text)
 	end
 end
